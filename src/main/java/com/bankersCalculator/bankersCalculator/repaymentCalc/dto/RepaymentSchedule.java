eb558d09cd1c7833c7fb1f543802c4453731e3dd
gw8413 <gw8413@gmail.com>
2024-07-03 22:19:21 +0900

    feat: 이자 계산 서비스 구현

---
+++ src/main/java/com/bankersCalculator/bankersCalculator/repaymentCalc/dto/RepaymentSchedule.java
@@ -0,0 +1,23 @@
+package com.bankersCalculator.bankersCalculator.repaymentCalc.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class RepaymentSchedule {
+
+    private int installmentNumber;
+    private double principalPayment;
+    private double interestPayment;
+    private double totalPayment;
+    private double remainingPrinciple;
+
+    @Builder
+    public RepaymentSchedule(int installmentNumber, double principalPayment, double interestPayment, double totalPayment, double remainingPrinciple) {
+        this.installmentNumber = installmentNumber;
+        this.principalPayment = principalPayment;
+        this.interestPayment = interestPayment;
+        this.totalPayment = totalPayment;
+        this.remainingPrinciple = remainingPrinciple;
+    }
+}
