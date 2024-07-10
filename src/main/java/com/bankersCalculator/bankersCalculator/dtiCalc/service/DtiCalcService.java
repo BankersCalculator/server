22e7b03f545be6a42f429624a7a5aa2e166299ba
myeongpil <audvlf92@naver.com>
2024-07-10 21:06:46 +0900

    feat. 산출결과 멘트수정

--- src/main/java/com/bankersCalculator/bankersCalculator/dtiCalc/service/DtiCalcService.java
+++ src/main/java/com/bankersCalculator/bankersCalculator/dtiCalc/service/DtiCalcService.java
@@ -5,6 +5,7 @@ import com.bankersCalculator.bankersCalculator.dtiCalc.dto.DtiCalcRequest;
 import com.bankersCalculator.bankersCalculator.dtiCalc.dto.DtiCalcResponse;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 import org.springframework.stereotype.Service;
 
@@ -14,41 +15,55 @@ public class DtiCalcService {
 	
     public DtiCalcResponse calculateDti(DtiCalcRequest request) {
     	 DtiCalcResponse response = new DtiCalcResponse();
+    	 DecimalFormat decimalFormat = new DecimalFormat("#,###");
          double annualPrincipalAndInterest;
-         String ctt = "";
-         // 1) 상환방식에 따른, 원리금 상환금액 계산 
+         String repaymentType = "" ;
+	     String ctt = "";	     
+	    
+         // 1) 기타대출 합산, 이자금액/ 기타대출의 이자율 (4% 가정)
+         double totalOtherDebtInterest = (request.getSavingsLoanAmount() + request.getOtherDebtAmount()) * 0.04;
+         
+         
+         // 2) 상환방식에 따른, 원리금 상환금액 계산 
          if ("equalPayment".equals(request.getRepaymentMethod())) {
-             annualPrincipalAndInterest = calculateEqualPayment(request.getApplicationAmount(), request.getInterestRate(), request.getLoanPeriod());
+        	 repaymentType = "원리금균등";
+             annualPrincipalAndInterest = calculateEqualPayment(request.getApplicationAmount() + request.getMortgageLoanAmount() - request.getRepaymentAmount(), request.getInterestRate(), request.getLoanPeriod());
+             
              
          } else if ("equalPrincipal".equals(request.getRepaymentMethod())) {
-             annualPrincipalAndInterest = calculateEqualPrincipal(request.getApplicationAmount(), request.getInterestRate(), request.getLoanPeriod());
+        	 repaymentType = "원금균등";
+             annualPrincipalAndInterest = calculateEqualPrincipal(request.getApplicationAmount() + request.getMortgageLoanAmount() - request.getRepaymentAmount(), request.getInterestRate(), request.getLoanPeriod());
          } else {
              throw new IllegalArgumentException("Invalid repayment method: " + request.getRepaymentMethod());
          }
          
-         // 2) 기타대출 합산, 이자금액/ 기타대출의 이자율 (4% 가정)
-         double totalOtherDebtInterest;
-         totalOtherDebtInterest = (request.getSavingsLoanAmount() + request.getMortgageLoanAmount() + request.getOtherDebtAmount()) * 0.04;
-         
          
          double dtiRatio = Math.round(((annualPrincipalAndInterest + totalOtherDebtInterest) / request.getAnnualIncome()) * 100 * 100)/100.0;
+         System.out.println(dtiRatio);  
+         System.out.println(Math.round(dtiRatio*100)/100.0);  
          
-         System.out.println(dtiRatio);  // 출력: "123.456"
-         System.out.println(Math.round(dtiRatio*100)/100.0);  // 출력: "123.456"
+         
+         System.out.println(decimalFormat.format(request.getApplicationAmount()));
+         
+         //decimalFormat.format(BigDecimal.valueOf(Math.round(request.getApplicationAmount())))
+         ctt = "주택담보대출 " + decimalFormat.format(request.getApplicationAmount())  + "원의 경우, <br>"  
+                 + "연 원리금상환금액은 " + decimalFormat.format(annualPrincipalAndInterest) + "원 이고 <br>" 
+                 + "(" + request.getLoanPeriod() + "년, " + request.getInterestRate() + "%, " + repaymentType + "기준)<br>"
+                 + "연소득은 " + decimalFormat.format(request.getAnnualIncome())+ "원 이므로<br>"
+                 + "DTI는 " + dtiRatio + "%로 예상됩니다."
+               ;
+        
+         
+         
+         
 
 
-         ctt = "주택담보대출 " + BigDecimal.valueOf(Math.round(request.getApplicationAmount()))  + "원의 경우, <br>"  
-                 + "연 원리금상환금액은 " + BigDecimal.valueOf(annualPrincipalAndInterest) + "원 이고 <br>" 
-                 + "(" + request.getLoanPeriod() + "년, " + request.getInterestRate() + "%, " + "원리금균등 기준)<br>"
-                 
-                 + "연소득은 " + BigDecimal.valueOf(Math.round(request.getAnnualIncome())) + "원 이므로<br>"
-                 + "DTI는 " + dtiRatio + "%로 예상됩니다.";
+         
          
          
          response.setCtt(ctt);
          response.setAnnnualPrincipalAndInterest(annualPrincipalAndInterest);
          response.setDtiRatio(dtiRatio);
-
          return response;
     }
     
