1b23ac65d5330f7d1c6e59e0fe2ead620163b288
myeongpil <audvlf92@naver.com>
2024-07-15 16:25:53 +0900

    refactor : Dti Controller 테스트완료. 인터페이스 대응 구현체 파일생성

--- src/main/java/com/bankersCalculator/bankersCalculator/dtiCalc/calculator/DtiCalculator.java
+++ src/main/java/com/bankersCalculator/bankersCalculator/dtiCalc/calculator/DtiCalculator.java
@@ -8,7 +8,9 @@ import com.bankersCalculator.bankersCalculator.repaymentCalc.service.RepaymentCa
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-
+/*
+ * 인터페이스 (Interface): 인터페이스는 클래스가 구현해야 하는 메서드들의 집합을 정의합니다. 인터페이스는 메서드의 시그니처(반환 타입, 메서드 이름, 매개변수 타입)를 정의하며, 실제 구현은 포함하지 않습니다.
+ */
 public interface DtiCalculator {
 	//Spring Framework에서 의존성 주입(Dependency Injection)을 위해 사용하는 어노테이션입니다. 이 어노테이션을 사용하면 스프링 컨테이너가 자동으로 해당 필드, 생성자, 또는 메서드에 필요한 의존성을 주입합니다. 이는 Spring의 IoC(Inversion of Control) 컨테이너가 관리하는 빈(Bean)들 사이의 의존성을 설정하는 데 사용됩니다.
 	
@@ -21,12 +23,12 @@ public interface DtiCalculator {
     
     
 	default DtiCalcResult calculateDti(DtiCalcServiceRequest.LoanStatus loanStatus) {
-		int maxTermForBullet = getMaxTermForBullet();
 		RepaymentType repaymentType = loanStatus.getRepaymentType();
 		DtiCalcResult dtiCalcResult = DtiCalcResult.builder().build();
 		
 		
 		if (repaymentType == RepaymentType.BULLET) {
+			int maxTermForBullet = getMaxTermForBullet();
 			dtiCalcResult = dtiCommonCalculator.dtiCalcForBulletLoan(loanStatus, maxTermForBullet);
 		} 
 		
--- src/test/java/com/bankersCalculator/bankersCalculator/dtiCalc/controller/DtiCalcControllerTest.java
+++ src/test/java/com/bankersCalculator/bankersCalculator/dtiCalc/controller/DtiCalcControllerTest.java
@@ -8,44 +8,75 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.bankersCalculator.bankersCalculator.common.enums.LoanType;
+import com.bankersCalculator.bankersCalculator.common.enums.RepaymentType;
 import com.bankersCalculator.bankersCalculator.dtiCalc.dto.DtiCalcResponse;
 import com.bankersCalculator.bankersCalculator.dtiCalc.service.DtiCalcService;
 
-public class DtiCalcControllerTest {
-	/*
-	
+// 어노테이션은 스프링 MVC 컨트롤러를 테스트하기 위해 사용됩니다. 이 어노테이션은 컨트롤러와 관련된 빈만 로드하여 테스트의 범위를 좁
+// 적용 대상: DtiCalcController.class를 지정하여 DtiCalcController를 테스트합니다.
+@WebMvcTest(DtiCalcController.class)
+public class DtiCalcControllerTest {	
+	//스프링 컨텍스트에서 관리되는 빈을 주입받기 위해 사용
 	@Autowired
     private MockMvc mockMvc;
-
+    
+	//용도: MockMvc를 사용하여 컨트롤러의 엔드포인트를 테스트합니다.
     @MockBean
-    private DtiCalcService dtiCalcService;
+    private DtiCalcService dtiCalcService; /// DtiCalcService의 모의 객체를 주입받아 사용
+    
+    
 
     @Test
     public void testShowCalcForm() throws Exception {
+    	//dti URL에 대해 get 요청 수행 
         mockMvc.perform(get("/dti"))
+                //HTTP 상태코드 200인지 확
                 .andExpect(status().isOk())
+                //반환 명칭 확
                 .andExpect(view().name("DtiCalc"))
+                //모델에 dtiCalcRequest 속성있는지 존재유무 확인 
                 .andExpect(model().attributeExists("dtiCalcRequest"));
     }
-
+    
+    
+    //목적: POST 요청을 통해 DTI 계산 결과를 처리하는 컨트롤러 메서드를 테스트합니다.
     @Test
     public void testDtiCalculate() throws Exception {
-        DtiCalcResponse response = new DtiCalcResponse();
-        
+    	// DtiCalcResponse는 빌더패턴을 사용하기 때문에, 빌더를 사용한 객체생성이 필요함.
+        //DtiCalcResponse response = new DtiCalcResponse();
+    	DtiCalcResponse response = DtiCalcResponse.builder()
+    		    .annualIncome(0)
+    		    .totalLoanCount(0)
+    		    .finalDtiRatio(0.0)
+    		    .dtiCalcResultList(new ArrayList<>())
+    		    .build();
+        //dtiCalcService.dticalculate 메서드가 호출될 때 모의 객체가 response를 반환하도록 설정
         when(dtiCalcService.dticalculate(any())).thenReturn(response);
-
+        
+        // /dti/result URL에 대해 POST 요청을 수행합니다
         mockMvc.perform(post("/dti/result")
-                .param("annualIncome", "1000000")
-                .param("loanStatusList[0].principal", "5000000"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("DtiCalc"))
-                .andExpect(model().attributeExists("dtiCalcResponse"));
-    }
-    */
 
+        
+        	.param("annualIncome", "1000000")
+        	.param("loanStatusList[0].repaymentType", RepaymentType.BULLET.name())
+        	.param("loanStatusList[0].loanType", LoanType.MORTGAGE.name())
+        	.param("loanStatusList[0].principal", "5000000")
+        	.param("loanStatusList[0].maturityPaymentAmount", "1000000")
+        	.param("loanStatusList[0].term", "30")
+        	.param("loanStatusList[0].gracePeriod", "5")
+        	.param("loanStatusList[0].interestRatePercentage", "3.5"))
+                 
+        	.andExpect(status().isOk())
+            .andExpect(view().name("DtiCalc"))
+            .andExpect(model().attributeExists("dtiCalcResponse"));
+    }
 }
