3ae17ca7709b31d6d7d150483a6a30860814c05b
gw8413 <gw8413@gmail.com>
2024-07-31 23:56:15 +0900

    feat: loanAdvise 시작

---
+++ src/main/java/com/bankersCalculator/server/loanAdvise/LoanAdvise.java
@@ -0,0 +1,14 @@
+package com.bankersCalculator.server.loanAdvise;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class LoanAdvise {
+
+
+
+}
