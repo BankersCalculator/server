4dd6f8d56fd8eb7cbd359878103d8c7b01055b80
gw8413 <gw8413@gmail.com>
2024-08-02 23:39:31 +0900

    feat: loanAdvise 스켈레톤 구현

--- src/main/java/com/bankersCalculator/server/advise/jeonseLoanAdvise/domain/LoanProduct.java
+++ src/main/java/com/bankersCalculator/server/advise/jeonseLoanAdvise/domain/LoanProduct.java
@@ -1,7 +1,14 @@
 package com.bankersCalculator.server.advise.jeonseLoanAdvise.domain;
 
-public class LoanProduct {
+public interface LoanProduct {
 
 
-    private int code;
+    // 개별 구현체에서 한도산출과 필터링을 구현하자.
+    String getProperty();
+
+    // TODO: filter 사유를 반환하는 걸로 수정 필요
+    boolean filtering();
+
+    double calculateLoanLimit();
+
 }
--- src/main/java/com/bankersCalculator/server/advise/jeonseLoanAdvise/service/JeonseProductFactory.java
+++
@@ -1,9 +0,0 @@
-package com.bankersCalculator.server.advise.jeonseLoanAdvise.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class JeonseProductFactory {
-    public JeonseProductFactory() {
-    }
-}
