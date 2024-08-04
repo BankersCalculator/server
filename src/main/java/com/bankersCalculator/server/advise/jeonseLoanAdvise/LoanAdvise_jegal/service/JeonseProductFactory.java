c9d65c234eb93d25296a331ffe07c34b825b7632
myeongpil <audvlf92@naver.com>
2024-08-04 21:18:10 +0900

    delete:loanAdvise_jegal 스켈레톤구현

--- src/main/java/com/bankersCalculator/server/advise/jeonseLoanAdvise/LoanAdvise_jegal/service/JeonseProductFactory.java
+++
@@ -1,8 +0,0 @@
-package com.bankersCalculator.server.advise.jeonseLoanAdvise.LoanAdvise_jegal.service;
-
-public class JeonseProductFactory {
-    public static List<JeonseProduct> getAllProducts() {
-        // 모든 상품 리스트 반환
-        return null;
-    }
-}
