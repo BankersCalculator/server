d8f85f541752c974346495019eac2234db38f845
myeongpil <audvlf92@naver.com>
2024-08-06 22:37:02 +0900

    주소검색 API 구현 ver0.1

--- src/main/java/com/bankersCalculator/server/addressSearch/service/AddressSearchService.java
+++ src/main/java/com/bankersCalculator/server/addressSearch/service/AddressSearchService.java
@@ -1,33 +1,82 @@
 package com.bankersCalculator.server.addressSearch.service;
 
-import com.bankersCalculator.server.addressSearch.dto.AddressSearchResponse;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.bankersCalculator.server.addressSearch.dto.AddressResponse;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AddressSearchService {
+    public List<AddressResponse> searchAddress(String keyword) throws IOException, JSONException {
+        String apiUrl = "https://business.juso.go.kr/addrlink/addrLinkApi.do";
+        String apiKey = "devU01TX0FVVEgyMDI0MDgwNTIxMTIzNjExNDk4OTM=";
+        String currentPage = "1"; //현재페이지번호
+        String countPerPage = "10"; //페이지당 출력할 Row 수
+        String resultType = "json"; //검색결과형 설정 (xml/json)
+        String apiUrlWithParams = apiUrl
+                                + "?confmKey=" + apiKey
+                                + "&currentPage=" + currentPage
+                                + "&countPerPage=" + countPerPage
+                                + "&resultType=" + resultType
+                                + "&keyword=" + URLEncoder.encode(keyword, "UTF-8");
 
-    private final RestTemplate restTemplate;
-    private final XmlMapper xmlMapper;
+        URL url = new URL(apiUrlWithParams);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
 
-    public AddressSearchService() {
-        this.restTemplate = new RestTemplate();
-        this.xmlMapper = new XmlMapper(); // Use XmlMapper for XML parsing
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        String line;
 
-    public AddressSearchResponse searchAddress(String keyword) {
-        String url = "https://business.juso.go.kr/addrlink/addrLinkApi.do?confmKey=devU01TX0FVVEgyMDI0MDgwNTIxMTIzNjExNDk4OTM=&currentPage=1&countPerPage=10&keyword="+keyword;
-
-        String response = restTemplate.getForObject(url, String.class);
-
-        System.out.println("API Response: " + response); // Print the XML response
-
-        try {
-            // Parse the XML response
-            return xmlMapper.readValue(response, AddressSearchResponse.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse response", e);
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
         }
+        br.close();
+
+        System.out.println(sb);
+
+        JSONObject jsonObject = new JSONObject(sb.toString());
+        JSONArray jusoArray = jsonObject.getJSONObject("results").getJSONArray("juso");
+
+        List<AddressResponse> addressList = new ArrayList<>();
+        for (int i = 0; i < jusoArray.length(); i++) {
+            JSONObject jusoObject = jusoArray.getJSONObject(i);
+            AddressResponse addressResponse = new AddressResponse();
+            addressResponse.setRoadAddr(jusoObject.getString("roadAddr"));
+            addressResponse.setJibunAddr(jusoObject.getString("jibunAddr"));
+            addressResponse.setZipNo(jusoObject.getString("zipNo"));
+            addressResponse.setAdmCd(jusoObject.getString("admCd"));
+            addressResponse.setRnMgtSn(jusoObject.getString("rnMgtSn"));
+            addressResponse.setBdMgtSn(jusoObject.getString("bdMgtSn"));
+            addressResponse.setDetBdNmList(jusoObject.getString("detBdNmList"));
+            addressResponse.setBdNm(jusoObject.getString("bdNm"));
+            addressResponse.setBdKdcd(jusoObject.getString("bdKdcd"));
+            addressResponse.setSiNm(jusoObject.getString("siNm"));
+            addressResponse.setSggNm(jusoObject.getString("sggNm"));
+            addressResponse.setEmdNm(jusoObject.getString("emdNm"));
+            addressResponse.setLiNm(jusoObject.getString("liNm"));
+            addressResponse.setRn(jusoObject.getString("rn"));
+            addressResponse.setUdrtYn(jusoObject.getString("udrtYn"));
+            addressResponse.setBuldMnnm(jusoObject.getInt("buldMnnm"));
+            addressResponse.setBuldSlno(jusoObject.getInt("buldSlno"));
+            addressResponse.setMtYn(jusoObject.getString("mtYn"));
+            addressResponse.setLnbrMnnm(jusoObject.getInt("lnbrMnnm"));
+            addressResponse.setLnbrSlno(jusoObject.getInt("lnbrSlno"));
+            addressResponse.setEmdNo(jusoObject.getString("emdNo"));
+            addressList.add(addressResponse);
+        }
+        return addressList;
     }
 }
