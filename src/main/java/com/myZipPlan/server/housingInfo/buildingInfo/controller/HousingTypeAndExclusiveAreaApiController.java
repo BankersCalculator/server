9448812f7b81ad8567540a3694a9e253a7b2a572
myeongpil <audvlf92@naver.com>
2024-09-22 18:55:38 +0900

    fix : housingInfo API / housingTypeAndExclusiveAreaApi -> housingTypeAndExclusiveArea 명칭수정

--- src/main/java/com/myZipPlan/server/housingInfo/buildingInfo/controller/HousingTypeAndExclusiveAreaApiController.java
+++ src/main/java/com/myZipPlan/server/housingInfo/buildingInfo/controller/HousingTypeAndExclusiveAreaApiController.java
@@ -3,20 +3,17 @@ package com.myZipPlan.server.housingInfo.buildingInfo.controller;
 import com.myZipPlan.server.common.api.ApiResponse;
 import com.myZipPlan.server.housingInfo.buildingInfo.api.HousingTypeAndExclusiveAreaApiClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/housingTypeAndExclusiveAreaApi")
+@RequestMapping("/api/v1/housingTypeAndExclusiveArea")
 @RequiredArgsConstructor
 public class HousingTypeAndExclusiveAreaApiController {
     private final HousingTypeAndExclusiveAreaApiClient housingTypeAndExclusiveAreaApiClient;
 
-    @PostMapping
+    @GetMapping
     public ApiResponse<Map<String, Object>> getHousingTypeAndExclusiveArea(
             @RequestParam String districtCodeFirst5,
             @RequestParam String districtCodeLast5,
