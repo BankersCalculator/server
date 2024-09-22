7378ded34313c893e91fb4392454b3ed5b2aed49
myeongpil <audvlf92@naver.com>
2024-09-22 22:47:01 +0900

    refactor : HousingTypeAndExclusiveArea API /  RequestParam -> RequestBody 변경

--- src/main/java/com/myZipPlan/server/housingInfo/buildingInfo/controller/HousingTypeAndExclusiveAreaApiController.java
+++ src/main/java/com/myZipPlan/server/housingInfo/buildingInfo/controller/HousingTypeAndExclusiveAreaApiController.java
@@ -2,6 +2,7 @@ package com.myZipPlan.server.housingInfo.buildingInfo.controller;
 
 import com.myZipPlan.server.common.api.ApiResponse;
 import com.myZipPlan.server.housingInfo.buildingInfo.api.HousingTypeAndExclusiveAreaApiClient;
+import com.myZipPlan.server.housingInfo.buildingInfo.dto.HousingTypeAndExclusiveAreaApiRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,17 +16,12 @@ public class HousingTypeAndExclusiveAreaApiController {
 
     @GetMapping
     public ApiResponse<Map<String, Object>> getHousingTypeAndExclusiveArea(
-            @RequestParam String districtCodeFirst5,
-            @RequestParam String districtCodeLast5,
-            @RequestParam String jibunMain,
-            @RequestParam String jibunSub
-
-            ) {
+            @RequestBody HousingTypeAndExclusiveAreaApiRequest request) {
         Map<String, Object> response = housingTypeAndExclusiveAreaApiClient.InquiryHousingTypeAndExclusiveArea(
-                districtCodeFirst5,
-                districtCodeLast5,
-                jibunMain,
-                jibunSub
+                request.getDistrictCodeFirst5(),
+                request.getDistrictCodeLast5(),
+                request.getJibunMain(),
+                request.getJibunSub()
         );
         return ApiResponse.ok(response);
     }
