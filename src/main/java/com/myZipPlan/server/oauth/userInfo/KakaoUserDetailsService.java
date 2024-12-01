f5117b956386860e943d1b8d0ce98ee6011072ae
myeongpil <audvlf92@naver.com>
2024-12-01 11:13:56 +0900

    fix : user profile / animalImageUrl > total count 12 -> 10 수정.

--- src/main/java/com/myZipPlan/server/oauth/userInfo/KakaoUserDetailsService.java
+++ src/main/java/com/myZipPlan/server/oauth/userInfo/KakaoUserDetailsService.java
@@ -26,7 +26,7 @@ public class KakaoUserDetailsService extends DefaultOAuth2UserService {
     private static final String PROVIDER = "KAKAO";
 
     private static final String ANIMAL_PROFILE_IMAGE_URL_TEMPLATE = "https://myzipplan-service-storage-dev.s3.ap-northeast-2.amazonaws.com/user-profile/comunity-profile-%02d.png";
-    private static final int ANIMAL_PROFILE_IMAGE_COUNT = 12;
+    private static final int ANIMAL_PROFILE_IMAGE_COUNT = 10;
     private static final List<String> ANIMAL_USERNAME_ADJECTIVES = List.of("포효하는", "눈썰매타는", "재테크천재", "독서하는", "춤추는", "밈잘알", "행복한", "느긋한", "용감한", "우아한", "청약노리는", "건물주꿈꾸는", "임대사업하는", "월세탈출하는", "아파트분양받는", "분산투자왕", "저축왕", "안전자산추구하는", "거시경제분석하는");
     private static final List<String> ANIMAL_USERNAME_ANIMALS = List.of("쿼카", "코끼리", "비버", "개구리", "호랑이", "펭귄", "다람쥐", "고양이", "여우", "사슴", "알파카", "사막여우", "바다표범", "돌고래", "치타", "늑대","미어캣" );
 
